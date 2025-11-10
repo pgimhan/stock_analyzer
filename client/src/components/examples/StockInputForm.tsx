14d38be4de8d72308719f9d57874f7fffa2fa69d
pgimhan <pravindugimhan@gmail.com>
2025-11-10 21:59:50 +0530

    code cleaning

--- client/src/components/examples/StockInputForm.tsx
+++
@@ -1,9 +0,0 @@
-import StockInputForm from '../StockInputForm';
-
-export default function StockInputFormExample() {
-  const handleAnalyze = (data: any) => {
-    console.log('Stock analysis data:', data);
-  };
-
-  return <StockInputForm onAnalyze={handleAnalyze} />;
-}
